f11c808fe7768543c79fbe8059e2af7dbe20faad
Pythonwithsean <pythonwithsean@gmail.com>
2025-01-18 20:57:43 +0000

    feat: collecting args

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 
-#define MIN_ARGS 10
+#define ARGS_LENGTH 10
 
 int main(int argc, char *argv[])
 {
 	// -h localhost -p 8080 -u pythonwithsean -p idk123 -db test
 	std::vector<char *> argsArr = {};
-	if (argc < MIN_ARGS)
+	if (argc != ARGS_LENGTH)
 	{
 		printf("[Error] Must Enter all args to connect to database! \n");
 		return 1;
